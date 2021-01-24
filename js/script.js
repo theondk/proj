eafa72b4519f8547bea9956dcc950502775f36be
Alex <partizan.pc7@gmail.com>
2021-01-24 15:19:02 +0300

    start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert(1);
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов', ''),
+      b = prompt ('На сколько вы его оцените', ''),
+      c = prompt('Один из последних просмотренных фильмов', ''),
+      d = prompt ('На сколько вы его оцените', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);   
